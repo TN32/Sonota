c9482dcefb616dd37aacbf00f7df28f26969cc3a
b7244 <b7244@oic.jp>
2020-01-14 18:59:31 +0900

    設定２

---
+++ app/src/main/java/com/example/sonota/ui/set/parttimejoblistdateclass.java
@@ -0,0 +1,59 @@
+package com.example.sonota.ui.set;
+
+public class parttimejoblistdateclass {
+    long id;
+    String name;
+    int hwage;
+    String pday;
+    String cday;
+
+    public parttimejoblistdateclass(long id, String name, int hwage, String pday, String cday) {
+        this.id = id;
+        this.name = name;
+        this.hwage = hwage;
+        this.pday = pday;
+        this.cday = cday;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHwage() {
+        return hwage;
+    }
+
+    public void setHwage(int hwage) {
+        this.hwage = hwage;
+    }
+
+    public String getPday() {
+        return pday;
+    }
+
+    public void setPday(String pday) {
+        this.pday = pday;
+    }
+
+    public String getCday() {
+        return cday;
+    }
+
+    public void setCday(String cday) {
+        this.cday = cday;
+    }
+
+
+}
